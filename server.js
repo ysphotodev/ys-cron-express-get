6736bbb696e989debd35b8ac258e2739f6ddeff1
ysphotodev <ysphotodev@gmail.com>
2024-05-27 00:21:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,61 @@
-const express = require('express');
-const axios = require('axios');
-const cron = require('node-cron');
+// const express = require('express');
+// const axios = require('axios');
+// const cron = require('node-cron');
+//
+// const app = express();
+// const port = 3000;
+//
+// // Function to make GET request
+// const makeGetRequest = async () => {
+//     try {
+//         const response = await axios.get('https://ys.od.ua/catalog');
+//         console.log('Response data:', response.data);
+//     } catch (error) {
+//         console.error('Error making GET request:', error);
+//     }
+// };
+//
+// // Schedule the GET request every 3 minutes
+// cron.schedule('*/3 * * * *', () => {
+//     console.log('Making GET request...');
+//     makeGetRequest();
+// });
+//
+// // Define a simple route
+// app.get('/', (req, res) => {
+//     res.send('Hello, world!');
+// });
+//
+// app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// });
 
-const app = express();
-const port = 3000;
 
-// Function to make GET request
-const makeGetRequest = async () => {
+
+
+const puppeteer = require('puppeteer');
+
+async function makeGetRequest(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
     try {
-        const response = await axios.get('https://strapi-ys-app-main.onrender.com/admin');
-        console.log('Response data:', response.data);
+        await page.goto(url, { waitUntil: 'networkidle2' });
+        console.log(`GET request to ${url} at ${new Date().toLocaleTimeString()}`);
     } catch (error) {
-        console.error('Error making GET request:', error);
+        console.error(`Failed to make GET request to ${url}:`, error);
+    } finally {
+        await browser.close();
     }
-};
+}
 
-// Schedule the GET request every 3 minutes
-cron.schedule('*/3 * * * *', () => {
-    console.log('Making GET request...');
-    makeGetRequest();
-});
+const url = 'https://strapi-ys-app-main.onrender.com'; // Replace with your target URL
+const interval = 1 * 60 * 1000; // 3 minutes in milliseconds
 
-// Define a simple route
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+// Immediately invoke the function and then set it to repeat at the specified interval
+(async function scheduleRequests() {
+    await makeGetRequest(url);
+    setInterval(async () => {
+        await makeGetRequest(url);
+    }, interval);
+})();
